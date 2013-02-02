10c9cd6dc843ddfca9f77c6546b57f2ce9fa1873
Dana Spisak <dana@luckyfishlab.com>
2013-02-02 00:10:31 -0600

    Init, all tests pass

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -158,19 +158,19 @@ Then /^I should see a successful sign up message$/ do
 end
 
 Then /^I should see an invalid email message$/ do
-  page.should have_content "Email is invalid"
+  page.should have_content "Emailis invalid"
 end
 
 Then /^I should see a missing password message$/ do
-  page.should have_content "Password can't be blank"
+  page.should have_content "Passwordcan't be blank"
 end
 
 Then /^I should see a missing password confirmation message$/ do
-  page.should have_content "Password doesn't match confirmation"
+  page.should have_content "Passworddoesn't match confirmation"
 end
 
 Then /^I should see a mismatched password message$/ do
-  page.should have_content "Password doesn't match confirmation"
+  page.should have_content "Passworddoesn't match confirmation"
 end
 
 Then /^I should see a signed out message$/ do
