5295928c19dac0f3f71dcdd47b0590f93f742e5c
Dana Spisak <dana@luckyfishlab.com>
2013-02-01 23:56:48 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sugar::Application.config.secret_token = 'b9fbf94feddc3ffe049728a30d2cd546661b4966c6914040ec782e752cdebc4c6060222197fbd6f1d24c6843de831da08e20ef992861ab85e64c386ad32930b4'
