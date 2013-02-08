3c662a7433a2277b176ae88737f95cea52fd675b
Dana Spisak <dana@luckyfishlab.com>
2013-02-08 08:51:00 -0600

    TRying to fix uploading

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Shoebox::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'shoebox.luckyfishlab.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
@@ -72,7 +72,7 @@ Shoebox::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "shoebox.luckyfishlab.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
