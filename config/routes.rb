e21bfcb35ed850e8dc11937e13e5910d53d5db05
Dana Spisak <dana@luckyfishlab.com>
2013-02-09 06:50:00 -0600

    Adding static pages for about and privacy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Shoebox::Application.routes.draw do
+
   resources :albums do
     resource :shares, only: [:create, :destroy,:new]
   end
@@ -12,5 +13,10 @@ Shoebox::Application.routes.draw do
   resources :users
   resources :shares, only: [:create, :destroy]
   resources :images
+
+  get "static_pages/home"
+  #get "static_pages/about"
+  match '/about' => 'static_pages#about'
+  match '/privacy_policy' => 'static_pages#privacy'
 
 end
