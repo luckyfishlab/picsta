02c8077616594ab5d2103151408eeff168ef7b2b
Dana Spisak <dana@luckyfishlab.com>
2013-02-09 16:41:45 -0600

    Added background stretched image:

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require jquery.backstretch.min
 //= require_tree .
+
+$('document').ready(function() {
+  $.backstretch('/assets/birthday.jpg');
+});
