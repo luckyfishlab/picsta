0c1ac4f0bafe4c84465fed6cf360acc904e85da9
Dana Spisak <dana@luckyfishlab.com>
2013-02-08 23:30:31 -0600

    Trying to fix template error

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -3,9 +3,9 @@ module ImagesHelper
     ret_text = ''
     album = record.album
     if Power.current.albums.include?(album)
-      ret_test << "#{link_to "Remove", @image, :confirm => 'Are you sure?', :method => :delete}"
-      ret_text.html_safe
+      ret_test << "#{link_to "Remove", @image, :confirm => 'Are you sure?', :method => :delete} | "
     end
+    ret_text.html_safe
   end
 
 end
