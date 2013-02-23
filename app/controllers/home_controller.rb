9b69afc9a737a36c8644f02655897348d23ebe8f
Dana Spisak <dana@luckyfishlab.com>
2013-02-23 00:23:34 -0600

    added activity feed at login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,8 @@ class HomeController < ApplicationController
         flash[:notice] = "Take a minute to add your name. Click Edit account in the top menu bar"
       end
     end
+    if user_signed_in?
+      @activities = PublicActivity::Activity.order("created_at desc")
+    end
   end
 end
