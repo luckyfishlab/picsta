6d3f36966aa1d4841af768499e381cb4bb4cc62b
Dana Spisak <dana@luckyfishlab.com>
2013-02-23 14:58:03 -0600

    improve copy

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -3,8 +3,8 @@ module AlbumsHelper
     ret_text = ''
     if Power.current.updatable_albums?(record)
       ret_text << "#{link_to 'Add Images', new_image_path(:album_id => record)}"
-      ret_text << " | #{link_to 'Edit', [:edit, record]}"
-      ret_text << " | #{link_to 'Share Album', new_album_shares_path(record)} | "
+      ret_text << " | #{link_to 'Edit', [:edit, record]} | "
+      #ret_text << " | #{link_to 'Share Album', new_album_shares_path(record)} | "
       ret_text.html_safe
     end
   end
