ac72574b747857812f45c61c0da7ed9c3ed960f2
Dana Spisak <dana@luckyfishlab.com>
2013-03-07 18:55:55 -0600

    ugh, disaster recovery only admin can use

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,6 +66,7 @@ class User < ActiveRecord::Base
   def update_stripe
     return if email.include?(ENV['ADMIN_EMAIL'])
     return if email.include?('@example.com') and not Rails.env.production?
+    return if true
     if customer_id.nil?
       if !stripe_token.present?
         raise "Stripe token not present. Can't create account."
