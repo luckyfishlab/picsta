109fee2a3a17ef1cb9e28b3c764d12c6f347814c
Dana Spisak <dana@luckyfishlab.com>
2013-03-09 15:26:40 -0600

    passing specs, registration closed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,42 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
+
   def new
-    @plan = params[:plan]
-    if @plan && ENV["ROLES"].include?(@plan) && @plan != "admin"
-      super
-    else
-      redirect_to root_path, :notice => 'Please select a subscription plan below.'
-    end
+    flash[:info] = 'Registration is not open yet, but please check back soon'
+    redirect_to root_path
   end
 
-  def update_plan
-    @user = current_user
-    #role = Role.find(params[:user][:role_ids]) unless params[:user][:role_ids].nil?
-    if false #@user.update_plan(role)
-      redirect_to edit_user_registration_path, :notice => 'Updated plan.'
-    else
-      flash.alert = 'Unable to update plan.'
-      render :edit
-    end
+  def create
+    flash[:info] = 'Registration is not open yet, but please check back soon'
+    redirect_to root_path
   end
 
-  def update_card
-    @user = current_user
-    @user.stripe_token = params[:user][:stripe_token]
-    if @user.save
-      redirect_to edit_user_registration_path, :notice => 'Updated card.'
-    else
-      flash.alert = 'Unable to update card.'
-      render :edit
-    end
-  end
-
-  private
-  def build_resource(*args)
-    super
-    if params[:plan]
-      ## This is 'rolify'
-      #resource.add_role(params[:plan])
-      resource.memberships << Role.find_by_name(params[:plan]).build_membership(resource)
-    end
-  end
 end
