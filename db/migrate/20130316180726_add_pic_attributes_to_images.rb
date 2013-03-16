ac6390bc01c35363ac5308a16d0af4e160c06367
Dana Spisak <dana@luckyfishlab.com>
2013-03-16 13:48:48 -0500

    migration failed

--- db/migrate/20130316180726_add_pic_attributes_to_images.rb
+++ db/migrate/20130316180726_add_pic_attributes_to_images.rb
@@ -1,9 +1,5 @@
 class AddPicAttributesToImages < ActiveRecord::Migration
   def change
     add_column :images, :file_size, :integer
-    Image.all.each do |image|
-      image.update_pic_attributes
-      image.save!
-    end
   end
 end
